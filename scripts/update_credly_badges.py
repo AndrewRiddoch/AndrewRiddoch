afb5c52213c3721fe4bc602c326eac16e8a2e276
Andrew Riddoch <36013054+AndrewRiddoch@users.noreply.github.com>
2026-09-14 13:00:31 +0100

    Change README path for Credly badges update script

--- scripts/update_credly_badges.py
+++ scripts/update_credly_badges.py
@@ -4,7 +4,7 @@ import sys
 import requests
 
 CREDLY_USERNAME = os.environ["CREDLY_USERNAME"]
-README_PATH = "README.md"
+README_PATH = "../README.md"
 START_MARKER = "<!-- CREDLY_BADGES_START -->"
 END_MARKER = "<!-- CREDLY_BADGES_END -->"
 
